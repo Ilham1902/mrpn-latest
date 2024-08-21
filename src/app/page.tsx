87f7eca11e59f868cbcf1774dbd7f8caa4dd2586
Agil Syofian Hidayat <agilsyofianh@gmail.com>
2024-08-21 09:55:55 +0700

    update gitignore

--- src/app/page.tsx
+++
@@ -1,6 +0,0 @@
-// import PageExecutiveSummary from "./executive-summary/page";
-import PageLogin from "./login/page";
-
-export default function Home() {
- return <PageLogin />;
-}
--- src/app/penetapan/konteks-strategis/form/add/page.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-
-import React from "react";
-import FormKonstra from "../partials/form";
-
-export default function PageFormKonteksStrategisAdd({}) {
- return <FormKonstra mode="add" />;
-}
